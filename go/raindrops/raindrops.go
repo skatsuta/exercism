d9818c4e57a182a92a462e3ff8fec16531f0cca6
Soshi Katsuta <katsuta_soshi@cyberagent.co.jp>
2015-07-20 14:01:35 +0900

    go/raindrops: rewrite more simply

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,11 +1,14 @@
 package raindrops
 
 import (
-	"math"
+	"bytes"
 	"strconv"
 )
 
-var words = []byte("PlingPlangPlong")
+var (
+	primes = [...]int{3, 5, 7}
+	words  = [...]string{"Pling", "Plang", "Plong"}
+)
 
 // Convert returns a string, the contents of which depends on the number's prime factors.
 //
@@ -15,54 +18,15 @@ var words = []byte("PlingPlangPlong")
 // - If the number does not contain 3, 5, or 7 as a prime factor,
 //   just pass the number's digits straight through.
 func Convert(n int) string {
-	primes := primeFactor(n)
-
-	b := make([]byte, 0, len(words))
-	for _, p := range primes {
-		switch {
-		case p%3 == 0:
-			b = append(b, words[0:5]...)
-		case p%5 == 0:
-			b = append(b, words[5:10]...)
-		case p%7 == 0:
-			b = append(b, words[10:15]...)
+	var buf bytes.Buffer
+	for i := 0; i < len(primes); i++ {
+		if n%primes[i] == 0 {
+			_, _ = buf.WriteString(words[i])
 		}
 	}
-	if len(b) == 0 {
+
+	if buf.Len() == 0 {
 		return strconv.Itoa(n)
 	}
-	return string(b)
-}
-
-func primeFactor(n int) []int {
-	ch := make(chan int)
-	m := int(math.Sqrt(float64(n)))
-	go generate(ch)
-	factors := make([]int, 0, m)
-	for i := 0; i <= m+1; i++ {
-		prime := <-ch
-		if n%prime == 0 {
-			factors = append(factors, prime)
-		}
-		ch1 := make(chan int)
-		go filter(ch, ch1, prime)
-		ch = ch1
-	}
-	return factors
-}
-
-// generate generates a integer sequence into channel.
-func generate(ch chan<- int) {
-	for i := 2; ; i++ {
-		ch <- i
-	}
-}
-
-func filter(in <-chan int, out chan<- int, prime int) {
-	for {
-		i := <-in
-		if i%prime != 0 {
-			out <- i
-		}
-	}
+	return buf.String()
 }
